6b388bba9a13f443b1d1c995265cee98a082ac08
Borislav Kosharov <bosakmaw@gmail.com>
2017-10-07 23:23:29 +0300

    IMPORT!!!

---
+++ ImportNMEA/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using NmeaParser;
+using NmeaParser.Nmea;
+using NmeaParser.Nmea.Gps;
+using CODE4TECH.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportNMEA
+{
+    class Program
+    {
+        const string Path = @"C:\Users\bosak\Desktop\GPS.txt";
+        const string ConnectionString = "Data Source=BOSAKPC;Database=Code4Tech;Integrated Security=True;Trusted_Connection=True;MultipleActiveResultSets=true";
+        const double KnotsToKmh = 1.852;
+
+        static void Main(string[] args)
+        {
+            var opt = new DbContextOptionsBuilder<Code4TechDbContext>().UseSqlServer(ConnectionString).Options;
+
+            using (var reader = new StreamReader(Path))
+            using (var ctx = new Code4TechDbContext(opt))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (line.StartsWith("$"))
+                    {
+                        var nmea = NmeaMessage.Parse(line);
+                        if (nmea is Gprmc msg)
+                        {
+                            var reading = new Reading()
+                            {
+                                DeviceId = 2323,
+                                Latitude = msg.Latitude,
+                                Longitude = msg.Longitude,
+                                Speed = msg.Speed * KnotsToKmh,
+                                Time = msg.FixTime
+                            };
+
+                            ctx.Readings.Add(reading);
+                        }
+                    }
+                }
+
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
