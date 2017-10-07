6f3de71e604053e66e2a28fd23d49e713b5b62d8
Borislav Kosharov <bosakmaw@gmail.com>
2017-10-07 12:23:43 +0300

    Add project files.

---
+++ CODE4TECH/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CODE4TECH.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
